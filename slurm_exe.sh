169bcdf67c889f56f5beb321e6a9f8c5f9b09d5c
hkhailee <42651565+hkhailee@users.noreply.github.com>
2021-11-05 11:45:20 -0600

    added nam-bio

--- slurm_exe.sh
+++ slurm_exe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J pretext # job name
 #SBATCH -o log_slurm.o%j # output and error file name (%j expands to jobID)
-#SBATCH -p gpu # queue (partition) -- defq, eduq, gpuq, shortq
+#SBATCH -p nam-bio # queue (partition) -- defq, eduq, gpuq, shortq, nam-bio
 #SBATCH -t 64:00:00 # run time (hh:mm:ss) - 12.0 hours in this example.
 
 module load slurm
@@ -10,8 +10,9 @@ module load gcc
 
 
 # Execute the program
+python moco.py --config_env configs/env.yml --config_exp configs/pretext/moco_rico.yml
 
-python simclr.py --config_env configs/env.yml --config_exp configs/pretext/moco_rico.yml
+#python simclr.py --config_env configs/env.yml --config_exp configs/pretext/moco_rico.yml
 #python scan.py --config_env configs/env.yml --config_exp configs/scan/scan_stl10.yml
 #python selflabel.py --config_env configs/env.yml --config_exp configs/selflabel/selflabel_cifar10.yml
 
